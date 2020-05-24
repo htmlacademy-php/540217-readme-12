655ca1ef6e1e621469bc05b6780030e47b2ebc8b
alex90ermakov <54851271+alex90ermakov@users.noreply.github.com>
2020-05-24 10:06:54 +0300

    Исправил ошибки

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 $is_auth = rand(0, 1);
 
-$user_name = ''; // укажите здесь ваше имя
+$user_name = 'Алексей'; // укажите здесь ваше имя
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -40,9 +40,8 @@ $user_name = ''; // укажите здесь ваше имя
             </div>
         </form>
         <div class="header__nav-wrapper">
-            <?php if ($is_auth == 1) {
-                # code...
-            print '<nav class="header__nav">
+            <?php if ($is_auth === 1): ?>
+             <nav class="header__nav">
                 <ul class="header__my-nav">
                     <li class="header__my-page header__my-page--popular">
                         <a class="header__page-link header__page-link--active" title="Популярный контент">
@@ -59,9 +58,7 @@ $user_name = ''; // укажите здесь ваше имя
                             <span class="visually-hidden">Личные сообщения</span>
                         </a>
                     </li>
-                </ul>';
-                }
-            ?>
+                </ul>
                 <ul class="header__user-nav">
                     <li class="header__profile">
                         <a class="header__profile-link" href="#">
@@ -70,7 +67,7 @@ $user_name = ''; // укажите здесь ваше имя
                             </div>
                             <div class="header__profile-name">
                                 <span>
-                                    <!--здесь должно быть имя пользователя-->
+                                    <?php print ("$user_name"); ?>
                                 </span>
                                 <svg class="header__link-arrow" width="10" height="6">
                                     <use xlink:href="#icon-arrow-right-ad"></use>
@@ -112,6 +109,7 @@ $user_name = ''; // укажите здесь ваше имя
                     </li>
                 </ul>
             </nav>
+            <?php endif; ?>
         </div>
     </div>
 </header>
